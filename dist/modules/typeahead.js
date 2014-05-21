abdb233422db8b86bb2b4b24c64d110d2fa7e241
Nick Perez <nickjperez@gmail.com>
2014-05-21 14:41:11 -0400

    chore(build): release

--- dist/modules/typeahead.js
+++ dist/modules/typeahead.js
@@ -215,12 +215,12 @@ angular.module('mgcrea.ngStrap.typeahead', [
             return element.val('');
           var index = typeahead.$getIndex(controller.$modelValue);
           var selected = '';
-          if (options.inputLabel && angular.isNumber(index)) {
+          if (options.inputLabel) {
             var getViewValue = $parse(options.inputLabel);
             selected = getViewValue(scope);
           } else {
             /* Get the label from the ng-options parser if it exists, otherwise use the viewValue (label property if viewValue is object) */
-            selected = angular.isDefined(index) ? typeahead.$scope.$matches[index].label : controller.$viewValue;
+            selected = angular.isNumber(index) ? typeahead.$scope.$matches[index].label : controller.$viewValue;
             if (angular.isObject(selected))
               selected = selected.label;
           }
